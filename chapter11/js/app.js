1b5ff3c8748b67161c7b011f72a9953a39d34662
TJ VanToll <tj.vantoll@gmail.com>
2014-02-25 09:20:17 -0500

    Saving off some updates to chapter 11 example.

--- chapter11/js/app.js
+++ chapter11/js/app.js
@@ -1,5 +1,5 @@
-require([ "text", "jquery", "underscore", "jquery-ui/autocomplete", "jquery-ui/button", "jquery-ui/datepicker", "jquery-ui/dialog", "jquery-ui/progressbar", "jquery-ui/selectmenu", "jquery-ui/spinner", "text!../template/flight-list.html" ],
-	function( text, $, _, autocomplete, button, datepicker, dialog, progressbar, selectmenu, spinner, flightListTemplate ) {
+require([ "text", "jquery", "underscore", "jquery-ui/autocomplete", "jquery-ui/button", "jquery-ui/datepicker", "jquery-ui/dialog", "jquery-ui/progressbar", "jquery-ui/selectmenu", "jquery-ui/spinner", "jquery-ui/tooltip", "text!../template/flight-list.html" ],
+	function( text, $, _, autocomplete, button, datepicker, dialog, progressbar, selectmenu, spinner, tooltip, flightListTemplate ) {
 
 	var fromAirport = $( "#from-airport" ),
 		toAirport = $( "#to-airport" ),
@@ -23,14 +23,11 @@ require([ "text", "jquery", "underscore", "jquery-ui/autocomplete", "jquery-ui/b
 		}
 
 		lookupAirports().then(function( data ) {
-			fromAirport.autocomplete({
-				source: data.airports,
-				minLength: 2
-			});
-			toAirport.autocomplete({
-				source: data.airports,
-				minLength: 2
-			});
+			fromAirport.add( toAirport )
+				.autocomplete({
+					source: data.airports,
+					minLength: 2
+				});
 		});
 
 		setupValidation();
@@ -44,10 +41,11 @@ require([ "text", "jquery", "underscore", "jquery-ui/autocomplete", "jquery-ui/b
 	};
 
 	function lookupAirports() {
-		return $.getJSON( "airports.json" );
+		return $.getJSON( "json/airports.json" );
 	};
 
 	function lookupFlights() {
+		return;
 		var selectedDate = $.datepicker.parseDate( "yy-mm-dd", date.val() );
 		return $.ajax({
 			headers: { "X-Mashape-Authorization": "CJOMaM8W7JgCCyHV5LO7wGzaHBo1jNfR" },
@@ -98,7 +96,7 @@ require([ "text", "jquery", "underscore", "jquery-ui/autocomplete", "jquery-ui/b
 		date.on( "change", function() {
 			var value;
 			try {
-				value = $.datepicker.parseDate( "mm/dd/yy", date.val() );
+				value = $.datepicker.parseDate( "yy-mm-dd", date.val() );
 			} catch ( error ) { }
 			if ( value ) {
 				date[ 0 ].setCustomValidity( "" );
@@ -108,14 +106,29 @@ require([ "text", "jquery", "underscore", "jquery-ui/autocomplete", "jquery-ui/b
 		});
 	};
 
+	// Handle for browsers without HTML5 constraint validation turned on.
+	function validateForm() {
+		var form = $( "form" );
+		form.find( ".ui-state-error-text" ).removeClass( "ui-state-error-text" );
+		form.find( ":invalid" ).each(function() {
+			form.find( "label[for=" + this.id + "]" )
+				.addClass( "ui-state-error-text" )
+			$( this ).attr( "aria-invalid", true )
+				.attr( "title", this.validationMessage )
+				.tooltip({ tooltipClass: "ui-state-error" });
+		}).first().focus();
+	};
+
 	function setupEvents() {
 		$( "form" ).on( "submit", function( event ) {
 			event.preventDefault();
-			processingDialog.dialog( "open" );
-			lookupFlights().then(function( data ) {
-				buildFlights( data );
-				processingDialog.dialog( "close" );
-			});
+			if ( validateForm() ) {
+				processingDialog.dialog( "open" );
+				lookupFlights().then(function( data ) {
+					buildFlights( data );
+					processingDialog.dialog( "close" );
+				});
+			}
 		});
 	};
 
