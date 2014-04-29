451f0303644168dc148fd6649a5b51b7475343b2
TJ VanToll <tj.vantoll@gmail.com>
2014-04-29 10:08:17 -0400

    Updating generated assets.

--- chapter10/built/js/build.js
+++ chapter10/built/js/build.js
@@ -1,1 +1,1 @@
-({appDir:"../",baseUrl:"js",dir:"../built",modules:[{name:"01-app"},{name:"02-app"}]});
+({appDir:"../",baseUrl:"js",dir:"../built",optimizeCss:"standard",modules:[{name:"01-app"},{name:"02-app"}]});
