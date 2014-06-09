888a5edb5678fa79309bff6f4e3b72053e911617
TJ VanToll <tj.vantoll@gmail.com>
2014-06-08 23:43:12 -0400

    Updating the version of declarative widgets

--- js/declarative-widgets.js
+++ js/declarative-widgets.js
@@ -9,46 +9,25 @@
 		factory( jQuery );
 	}
 }(function( $ ) {
-	var capitals = /[A-Z]/g,
-		buildAttribute = function( c ) {
-			return "-" + c.toLowerCase();
-		},
-		stringToType = function( data ) {
-
-			// Copied from core's src/data.js:dataAttr()
-			var rbrace = /(?:\{[\s\S]*\}|\[[\s\S]*\])$/;
-			try {
-				data = data === "true" ? true :
-				data === "false" ? false :
-				data === "null" ? null :
-
-				// Only convert to a number if it doesn't change the string
-				+data + "" === data ? +data :
-				rbrace.test( data ) ? JSON.parse( data ) :
-				data;
-			} catch( err ) {}
-			
-			return data;
-		},
-		plugin = {
-			enhance: function() {
-				this.find( "[data-role]" ).addBack( "[data-role]" ).each(function() {
-					var element = $( this ),
-						role = element.attr( "data-role" );
-					element[ role ]();
-				});
-				return this;
-			}
-		};
+	var plugin = {
+		enhance: function() {
+			this.find( "[data-role]" ).addBack( "[data-role]" ).each(function() {
+				var element = $( this ),
+					role = element.attr( "data-role" );
+				element[ role ]();
+			});
+			return this;
+		}
+	};
 
 	$.extend( $.Widget.prototype, {
 		_getCreateOptions: function() {
-			var option, value,
+			var option,
+				value,
 				options = {};
 
 			for ( option in this.options ) {
-				value = this.element.attr( "data-" + option.replace( capitals, buildAttribute ) );
-				value = stringToType( value );
+				value = this.element.data( option );
 				if ( value != null ) {
 					options[ option ] = value;
 				}
